2a99ba81e4e62d41dc4d70e1ed58da7f5eda2183
pablosalina25 <casuarioventas@gmail.com>
2024-04-20 15:12:17 -0300

    task 4

---
+++ models/api/v1/views/index.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from api.v1.views import app_views
+import json
+
+@app_views.route('/status')
+def status():
+    dict = {
+        "status": "OK"
+    }
+    return json.dumps(dict)
