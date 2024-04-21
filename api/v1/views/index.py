4c64caa9b1bb23a4c49532c8e071f6bc8e83b943
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-04-21 19:00:04 +0000

    update file

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,12 +3,6 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 
 
 @app_views.route('/status', methods=['GET'])
@@ -20,6 +14,13 @@ def status():
 @app_views.route('/api/v1/stats', methods=['GET'])
 def api_stats():
     """stats"""
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+
     stats = {
         "amenities": storage.count(Amenity),
         "cities": storage.count(City),
