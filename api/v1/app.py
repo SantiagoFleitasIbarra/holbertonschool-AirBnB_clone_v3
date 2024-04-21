4844a2d9c2060586fa460335e2a8429196cdfe7f
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-04-21 20:03:32 +0000

    add task

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 
 
 from api.v1.views import app_views
-from flask import Flask, jsonify
+from flask import Flask, make_response, jsonify
 from models import storage
 
 app = Flask(__name__)
@@ -16,10 +16,10 @@ def teardown_appcontext():
     storage.close()
 
 
-@app.errorhandler(404, methods=['GET'])
+@app.errorhandler(404)
 def not_found(error):
     """Not found"""
-    return jsonify({"error": "Not found"}), 404
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
