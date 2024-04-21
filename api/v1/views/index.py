7b824a5e8edb4f274ac781a324aebd149d37c92e
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-04-21 18:45:57 +0000

    add task

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,16 @@
 """Index.py"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
 def status():
     """status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/api/v1/stats', methods=['GET'])
+def api_stats():
+    """stats"""
+    storage.count()
