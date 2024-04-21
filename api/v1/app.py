079fcfc170ee53a93d0dcfe518287e93b8740af6
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-04-21 18:18:21 +0000

    update files

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,21 +3,21 @@
 
 
 from flask import Flask
-from models import storage
 from api.v1.views import app_views
-import os
+from models import storage
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+app.register_blueprint(app_views, url_prefix='/api/v1')
 
 
 @app.teardown_appcontext
-def teardown_appcontext():
+def teardown_appcontext(exception):
     """Calls storage.close()"""
     storage.close()
 
 
 if __name__ == "__main__":
+    import os
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = os.getenv('HBNB_API_PORT', 5000)
     app.run(host=host, port=port, threaded=True)
